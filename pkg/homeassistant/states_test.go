2dd9355e0ae95f246016528cf46b0d672550f82c
David Thorpe <djt@mutablelogic.com>
2024-03-31 09:48:04 +0100

    Updated

--- pkg/homeassistant/states_test.go
+++ pkg/homeassistant/states_test.go
@@ -22,3 +22,42 @@ func Test_states_001(t *testing.T) {
 
 	t.Log(states)
 }
+
+func Test_states_002(t *testing.T) {
+	assert := assert.New(t)
+	client, err := homeassistant.New(GetEndPoint(t), GetApiKey(t), opts.OptTrace(os.Stderr, true))
+	assert.NoError(err)
+	assert.NotNil(client)
+
+	sensors, err := client.Sensors()
+	assert.NoError(err)
+	assert.NotNil(sensors)
+
+	t.Log(sensors)
+}
+
+func Test_states_003(t *testing.T) {
+	assert := assert.New(t)
+	client, err := homeassistant.New(GetEndPoint(t), GetApiKey(t), opts.OptTrace(os.Stderr, true))
+	assert.NoError(err)
+	assert.NotNil(client)
+
+	lights, err := client.Lights()
+	assert.NoError(err)
+	assert.NotNil(lights)
+
+	t.Log(lights)
+}
+
+func Test_states_004(t *testing.T) {
+	assert := assert.New(t)
+	client, err := homeassistant.New(GetEndPoint(t), GetApiKey(t), opts.OptTrace(os.Stderr, true))
+	assert.NoError(err)
+	assert.NotNil(client)
+
+	actuators, err := client.Actuators()
+	assert.NoError(err)
+	assert.NotNil(actuators)
+
+	t.Log(actuators)
+}
